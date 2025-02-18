3c2cf2628fb6373c1b29a2f4d764f104f4444e3a
CaioBattisti <cluizbattisti@gmail.com>
2025-02-18 19:44:31 -0300

    atualização

---
+++ atividade1_CaioLuizBattisti.py
@@ -0,0 +1,3 @@
+#faça um programa que leia os numeros digitados pelo usuario, a cada numero digitado ele deve ser somado ao 
+#anterior e a cada soma exibida na tela. quando a soma for superior
+#a 50 ele deve exibir a mensagem "o total e:" e exibir o total e parar o programa.
---
+++ atividade2_CaioLuizBattisti.py
@@ -0,0 +1,2 @@
+#peça ao usuario para inserir um numero. continue perguntado ate que ele insira 5 numeros e em seguida exiba a mensagem
+#"o ultmo numero que voce digitou foi:" oultimo numero que o usuario digitar
