6a0460b3db937aa435a5afafa083fc702b73b62a
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 20:18:51 -0300

    finalizado 2

--- atividade2_CaioLuizBattisti.py
+++ atividade2_CaioLuizBattisti.py
@@ -1,3 +1,7 @@
 #peça ao usuario para inserir um numero. continue perguntado ate que ele insira 5 numeros e em seguida exiba a mensagem
 #"o ultimo numero que voce digitou foi:" o ultimo numero que o usuario digitar
-num =int(input("Digite um numero: "))
+numeros = []
+while len(numeros) < 5:
+    num =int(input("Digite um numero: "))
+    numeros.append(num)
+print("O ultimo numero que voce digitou foi: ",(numeros[-1]))
