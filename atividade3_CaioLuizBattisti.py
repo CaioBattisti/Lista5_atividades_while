72575c5780b8aa8d5ce9afc78560ac8ad5b884b6
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 20:50:25 -0300

    finalizado 3

--- atividade3_CaioLuizBattisti.py
+++ atividade3_CaioLuizBattisti.py
@@ -1,4 +1,6 @@
-#
+#Peça ao usuário para inserir um número e, em seguida, insira outro número. Some esses dois números e pergunte se ele quer adicionar outro número.
+#Se ele digitar “ s ", diga para inserir outro número e continuar adicionando números e somando até que ele não respondam “ s ".
+#Depois que o loop for interrompido, exiba o total.
 soma = 0
 while True:
     num=int(input("digite um numero: "))
