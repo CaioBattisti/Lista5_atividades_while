eb9fb03c22b68622d3217311918badfad01c661d
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 20:47:45 -0300

    finalizado a 4

---
+++ atividade4_CaioLuizBattisti.py
@@ -0,0 +1,11 @@
+#Faça um programa que peça o nome do convidado que o usuario deseja convidar para uma festa.
+#Depois disso, exiba a mensagem "[nome] foi adicionado(a) com sucesso no convite!" e adicione 1 à contagem.
+convidados = []
+while True:
+    nome =input("Digite o nome do convidado: ")
+    convidados.append(nome)
+    print(f"{nome} foi adicionado(a) com sucesso no convite!")
+    continuar =input("voce deseja convidar outra pessoa? (s/n): ").strip().lower()
+    if continuar != "s":
+        print("O total de convidados é:",convidados)
+        break
