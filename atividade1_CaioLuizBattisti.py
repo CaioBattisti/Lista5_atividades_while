fcdcaa57844cc97788ccbb8aaabc5b1db310f106
CaioBattisti <cluizbattisti@gmail.com>
2025-02-20 20:12:10 -0300

    finalizado 1

--- atividade1_CaioLuizBattisti.py
+++ atividade1_CaioLuizBattisti.py
@@ -1,12 +1,9 @@
 #faça um programa que leia os numeros digitados pelo usuario, a cada numero digitado ele deve ser somado ao 
 #anterior e a cada soma exibida na tela. quando a soma for superior
 #a 50 ele deve exibir a mensagem "o total e:" e exibir o total e parar o programa.
-numeros = 0
-while True:
-    num1 =int(input("Digite um numero"))
-    numeros += num1
-    if numeros > 50:
-        print("O total é:",numeros)
-    break
-else:
-    print("Soma parcial:",numeros)
+total = 0
+while total < 50:
+    numero =int(input("Digite um numero:"))
+    total += numero
+    print("Soma atual: ",total)
+print("o total é: ",total)
